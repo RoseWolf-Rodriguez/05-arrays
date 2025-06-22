61a44bd4b44806efd03c26a9c4c6dc773d061fe5
Lucretia Teretto <lteretto@asu.edu>
2025-06-22 02:55:17 +0000

    added movie titles

--- script.js
+++ script.js
@@ -1,19 +1,59 @@
+// Arrays of movie titles for each genre
+// Comedy movies
+const comedyMovies = [
+  "The Mask",
+  "Superbad",
+  "Dumb and Dumber",
+  "Step Brothers"
+];
+
+// Action movies
+const actionMovies = [
+  "Die Hard",
+  "Mad Max: Fury Road",
+  "John Wick",
+  "The Dark Knight"
+];
+
+// Drama movies
+const dramaMovies = [
+  "Forrest Gump",
+  "The Shawshank Redemption",
+  "The Godfather",
+  "A Beautiful Mind"
+];
+
+// Sci-Fi movies
+const sciFiMovies = [
+  "Inception",
+  "The Matrix",
+  "Interstellar",
+  "Blade Runner 2049"
+];
+
 // Set up event listener for when the user selects a genre
 document.getElementById("genre").addEventListener("change", function() {
   // Get the selected genre
   const selectedGenre = document.getElementById("genre").value;
 
-  // Add movies to the movieList based on the selected genre
+  // Create a variable to hold the list of movies
+  let movieList = [];
+
+  // Assign the correct movie array to movieList based on the selected genre
   if (selectedGenre === "Comedy") {
-    console.log("You selected 'Comedy'");
+    // If Comedy is selected, use the comedyMovies array
+    movieList = comedyMovies;
   } else if (selectedGenre === "Action") {
-    console.log("You selected 'Action'");
+    // If Action is selected, use the actionMovies array
+    movieList = actionMovies;
   } else if (selectedGenre === "Drama") {
-    console.log("You selected 'Drama'");
+    // If Drama is selected, use the dramaMovies array
+    movieList = dramaMovies;
   } else if (selectedGenre === "Sci-Fi") {
-    console.log("You selected 'Sci-Fi'");
+    // If Sci-Fi is selected, use the sciFiMovies array
+    movieList = sciFiMovies;
   }
 
   // Display the list of movies on the page
-  document.getElementById("movieRecommendations").innerText = `Enjoy: !`;
+  document.getElementById("movieRecommendations").innerText = `Enjoy: ${movieList.join(", ")}`;
 });
