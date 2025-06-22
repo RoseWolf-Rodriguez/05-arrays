9234f118fe51fee1884def209e3024e8065ba71a
Lucretia Teretto <lteretto@asu.edu>
2025-06-22 02:59:41 +0000

    added .push

--- script.js
+++ script.js
@@ -1,35 +1,4 @@
-// Arrays of movie titles for each genre
-// Comedy movies
-const comedyMovies = [
-  "The Mask",
-  "Superbad",
-  "Dumb and Dumber",
-  "Step Brothers"
-];
 
-// Action movies
-const actionMovies = [
-  "Die Hard",
-  "Mad Max: Fury Road",
-  "John Wick",
-  "The Dark Knight"
-];
-
-// Drama movies
-const dramaMovies = [
-  "Forrest Gump",
-  "The Shawshank Redemption",
-  "The Godfather",
-  "A Beautiful Mind"
-];
-
-// Sci-Fi movies
-const sciFiMovies = [
-  "Inception",
-  "The Matrix",
-  "Interstellar",
-  "Blade Runner 2049"
-];
 
 // Set up event listener for when the user selects a genre
 document.getElementById("genre").addEventListener("change", function() {
@@ -39,19 +8,31 @@ document.getElementById("genre").addEventListener("change", function() {
   // Create a variable to hold the list of movies
   let movieList = [];
 
-  // Assign the correct movie array to movieList based on the selected genre
+  // Assign the correct movie titles to movieList based on the selected genre
   if (selectedGenre === "Comedy") {
-    // If Comedy is selected, use the comedyMovies array
-    movieList = comedyMovies;
+    // If Comedy is selected, add each comedy movie to movieList
+    movieList.push("The Mask");
+    movieList.push("Superbad");
+    movieList.push("Dumb and Dumber");
+    movieList.push("Step Brothers");
   } else if (selectedGenre === "Action") {
-    // If Action is selected, use the actionMovies array
-    movieList = actionMovies;
+    // If Action is selected, add each action movie to movieList
+    movieList.push("Die Hard");
+    movieList.push("Mad Max: Fury Road");
+    movieList.push("John Wick");
+    movieList.push("The Dark Knight");
   } else if (selectedGenre === "Drama") {
-    // If Drama is selected, use the dramaMovies array
-    movieList = dramaMovies;
+    // If Drama is selected, add each drama movie to movieList
+    movieList.push("Forrest Gump");
+    movieList.push("The Shawshank Redemption");
+    movieList.push("The Godfather");
+    movieList.push("A Beautiful Mind");
   } else if (selectedGenre === "Sci-Fi") {
-    // If Sci-Fi is selected, use the sciFiMovies array
-    movieList = sciFiMovies;
+    // If Sci-Fi is selected, add each sci-fi movie to movieList
+    movieList.push("Inception");
+    movieList.push("The Matrix");
+    movieList.push("Interstellar");
+    movieList.push("Blade Runner 2049");
   }
 
   // Display the list of movies on the page
